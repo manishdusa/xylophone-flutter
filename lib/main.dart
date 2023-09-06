1107ada59081caca4f43412ee6ee09cdca07f63f
Manish Dusa <manish.dusa211026@tsecol.onmicrosoft.com>
2023-09-06 11:54:43 +0530

    A xylophone app
    Introducing the Xylophone Flutter app, a delightful musical experience right at your fingertips! This innovative app recreates the enchanting sounds of a xylophone, allowing users to explore their musical talents with ease. With a user-friendly interface and beautifully designed virtual xylophone keys, Xylophone lets you create melodies, play your favorite songs, or simply unwind with soothing tunes. Choose from a variety of vibrant tones and scales to customize your musical journey. Whether you're a novice or a seasoned musician, Xylophone offers a fun and interactive way to explore the world of xylophone music on your mobile device. Start composing your melodies and share your musical creations with friends - all with Xylophone, the ultimate xylophone experience in the palm of your hand.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,58 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void play(int soundnumber)
+  {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundnumber.wav'));
+  }
+
+
+
+  Expanded buildKey(Color backgroundColor,int soundnumber)
+  {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: (){
+          play(soundnumber);
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+        ),
+      ),
+    );
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  buildKey(Colors.redAccent,1),
+                  buildKey(Colors.orange,2),
+                  buildKey(Colors.green,3),
+                  buildKey(Colors.yellow,4),
+                  buildKey(Colors.purple,5),
+                  buildKey(Colors.deepPurple,6),
+                  buildKey(Colors.lightBlue,7),
+
+
+                ],
+              )),
         ),
-      ),
+          ),
+
     );
   }
 }
